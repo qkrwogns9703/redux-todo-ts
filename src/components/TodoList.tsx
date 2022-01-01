67fa68160a6842e398d4e300f9c7ee03c71b91f1
qkrwogns9703 <jaehunb350@gmail.com>
2022-01-01 22:12:27 +0900

    render 리팩토링

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -8,18 +8,23 @@ type TodoListProps = {
 };
 
 function TodoList({ todos, onToggle, onRemove }: TodoListProps) {
-	if (todos.length === 0) return <p>등록된 항목이 없습니다.</p>;
 	return (
-		<ul>
-			{todos.map(todo => (
-				<TodoItem
-					todo={todo}
-					onToggle={onToggle}
-					onRemove={onRemove}
-					key={todo.id}
-				/>
-			))}
-		</ul>
+		<>
+			{!todos.length ? (
+				<p>등록된 항목이 없습니다.</p>
+			) : (
+				<ul>
+					{todos.map(todo => (
+						<TodoItem
+							todo={todo}
+							onToggle={onToggle}
+							onRemove={onRemove}
+							key={todo.id}
+						/>
+					))}
+				</ul>
+			)}
+		</>
 	);
 }
 
